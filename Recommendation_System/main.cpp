da60b4ebe596e3da55211eefec8ac9815331dba0
Kazi Sahib <161735924+ksahib@users.noreply.github.com>
2024-09-10 21:27:59 +0600

    Update main.cpp

--- Recommendation_System/main.cpp
+++ Recommendation_System/main.cpp
@@ -10,6 +10,7 @@
 #include <set>
 #include <chrono>
 #include <random>
+#include <conio.h>
 
 typedef struct {
     std::string name;
@@ -32,7 +33,7 @@ typedef struct {
     std::vector<unsigned> d; // d[l]: degrees of G_l
     std::vector<unsigned> cd; // cumulative degree: (starts with 0) length=n+1
     std::vector<unsigned> adj; // truncated list of neighbors
-    std::vector<unsigned> lab; // lab[i] label of node i
+    std::vector<unsigned> lab; // label of nodes
     std::vector<std::vector<unsigned>> sub; // sub[l]: nodes in G_l
     unsigned n() const {
         return nodes.size(); // no. of nodes
@@ -41,6 +42,8 @@ typedef struct {
         return edges.size(); // no. of edges
     }
 } graph;
+
+//builds the nodes of the graph from the dataset
 void buildGraph(graph &g) {
     node n;
     std::ifstream file("playlist_audio_features.txt");
@@ -88,6 +91,7 @@ void buildGraph(graph &g) {
     file.close();
 }
 
+//creates edges based on the mood parameter
 void readEdgelist(graph&g, int mood)
 {
     std::string filename = "graph" + std::to_string(mood) + ".txt";
@@ -102,9 +106,11 @@ void readEdgelist(graph&g, int mood)
     }
 }
 
+//generates the adjacency list of the graph
 void makeAdj(graph &g) {
-    unsigned md = 0;
+    unsigned md = 0; // max degree
     std::vector<unsigned> d0(g.n(), 0);
+    //calculates degree
     for (auto &e : g.edges) {
         d0[e.source]++;
         d0[e.target]++;
@@ -112,31 +118,36 @@ void makeAdj(graph &g) {
     for (auto &x : d0) {
         md = std::max(x, md);
     }
+    //initialisation for bin sort
     std::vector<unsigned> bin(md + 1, 0);
     std::vector<unsigned> pos(g.n());
     std::vector<unsigned> vert(g.n());
+    //calculates the frequency of the degrees
     for (unsigned i = 0; i < g.n(); i++) {
         bin[d0[i]]++;
     }
+    //calculates the starting position of the degrees
     unsigned start = 0;
     for (unsigned i = 0; i <= md; i++) {
         unsigned num = bin[i];
         bin[i] = start;
         start += num;
     }
+    //calculates positions of the nodes in the vert array
     for (unsigned i = 0; i < g.n(); i++) {
         pos[i] = bin[d0[i]];
-        vert[pos[i]] = i;
+        vert[pos[i]] = i; // contains the nodes in increasing order of degree
         bin[d0[i]]++;
     }
     g.d = vert;
-    std::vector<unsigned> cd0(g.n() + 1, 0);
+    std::vector<unsigned> cd0(g.n() + 1, 0); 
     std::vector<unsigned> adj0(2 * g.e(), 0);
     g.adj.resize(2 * g.e());
     for (unsigned i = 1; i < g.n() + 1; i++) {
-        cd0[i] = cd0[i - 1] + d0[i - 1];
-        d0[i - 1] = 0;
+        cd0[i] = cd0[i - 1] + d0[i - 1]; //cumulative degree
+        d0[i - 1] = 0; //sets degrees to 0
     }
+    //calculates adjacency: stores them in a way where cd stores the starting position and degree determines the number of spaces it has reserved for its neighbors
     for (unsigned i = 0; i < g.e(); i++) {
         adj0[cd0[g.edges[i].source] + d0[g.edges[i].source]++] = g.edges[i].target;
         adj0[cd0[g.edges[i].target] + d0[g.edges[i].target]++] = g.edges[i].source;
@@ -145,6 +156,7 @@ void makeAdj(graph &g) {
     g.cd = cd0;
 }
 
+//returns a set of neighbors
 std::unordered_set<int> neighbors(const graph &g, unsigned n) {
     std::unordered_set<int> neighbor;
     unsigned start = g.cd[n];
@@ -158,11 +170,13 @@ std::unordered_set<int> neighbors(const graph &g, unsigned n) {
     return neighbor;
 }
 
+//implementation of the bron kerbosch pivot algorithm. Finds the maximal cliques in a graph
 void bronKerboschPivot(const graph& g, 
                        std::unordered_set<int>& R, 
                        std::unordered_set<int>& P, 
                        std::unordered_set<int>& X, 
                        std::vector<std::unordered_set<int>>& cliques) {
+    //base case: reports cliques when both P and X are empty
     if (P.empty() && X.empty()) {
         cliques.push_back(R);
         return;
@@ -178,7 +192,7 @@ void bronKerboschPivot(const graph& g,
         {
             if(P_union_X.find(g.d[i]) != P_union_X.end())
             {
-                pivot = g.d[i];
+                pivot = g.d[i]; // chooses the highest degree node as the pivot
                 break;
             }
         }
@@ -197,15 +211,15 @@ void bronKerboschPivot(const graph& g,
         std::unordered_set<int> P_inter_Nv, X_inter_Nv;
 
         std::set_intersection(P.begin(), P.end(), N_v.begin(), N_v.end(),
-                              std::inserter(P_inter_Nv, P_inter_Nv.begin()));
+                              std::inserter(P_inter_Nv, P_inter_Nv.begin())); // P intersection N(v)
         std::set_intersection(X.begin(), X.end(), N_v.begin(), N_v.end(),
-                              std::inserter(X_inter_Nv, X_inter_Nv.begin()));
+                              std::inserter(X_inter_Nv, X_inter_Nv.begin())); // X intersection N(v)
 
-        bronKerboschPivot(g, R, P_inter_Nv, X_inter_Nv, cliques);
+        bronKerboschPivot(g, R, P_inter_Nv, X_inter_Nv, cliques); // recursive call
 
-        R.erase(v);
-        P.erase(v);
-        X.insert(v);
+        R.erase(v); // erase v from the candidate set
+        P.erase(v); //erase v from the unexplored set
+        X.insert(v); // insert v to the explored set
     }
 }
 
@@ -213,6 +227,93 @@ bool fileExists(const std::string &filename)
 {
     std::ifstream file(filename);
     return file.good();
+}
+
+void addToPlaylist(graph &g, int i)
+{
+    std::string filename = "playlist.txt";
+    std::ofstream file;
+    file.open(filename, std::ios_base::app);
+    file << g.nodes[i].name << "-" << g.nodes[i].artist << std::endl;
+
+}
+
+void loadPlaylist()
+{
+    std::ifstream file("playlist.txt");
+    std::string line;
+    while (std::getline(file, line)) {
+        std::cout << line << std::endl;
+    }
+}
+
+void loadLogo()
+{
+    std::string logo =  R"(
+                                                                                                    
+       ,--.                                                                                         
+   ,--/  /|              .--.--.        ___                                                  ____   
+,---,': / '             /  /    '.    ,--.'|_                                              ,'  , `. 
+:   : '/ /      ,---,. |  :  /`. /    |  | :,'    __  ,-.                               ,-+-,.' _ | 
+|   '   ,     ,'  .' | ;  |  |--`     :  : ' :  ,' ,'/ /|                            ,-+-. ;   , || 
+'   |  /    ,---.'   , |  :  ;_     .;__,'  /   '  | |' |    ,---.      ,--.--.     ,--.'|'   |  || 
+|   ;  ;    |   |    |  \  \    `.  |  |   |    |  |   ,'   /     \    /       \   |   |  ,', |  |, 
+:   '   \   :   :  .'    `----.   \ :__,'| :    '  :  /    /    /  |  .--.  .-. |  |   | /  | |--'  
+|   |    '  :   |.'      __ \  \  |   '  : |__  |  | '    .    ' / |   \__\/: . .  |   : |  | ,     
+'   : |.  \ `---'       /  /`--'  /   |  | '.'| ;  : |    '   ;   /|   ," .--.; |  |   : |  |/      
+|   | '_\.'            '--'.     /    ;  :    ; |  , ;    '   |  / |  /  /  ,.  |  |   | |`-'       
+'   : |                  `--'---'     |  ,   /   ---'     |   :    | ;  :   .'   \ |   ;/           
+;   |,'                                ---`-'              \   \  /  |  ,     .-./ '---'            
+'---'                                                       `----'    `--`---'                      
+                                                                                                    
+)";
+    std::cout << "\t\t\t\t" << logo << std::endl;
+}
+
+// main UI
+int home(graph &g, std::vector<int> &prev)
+{
+    system("cls");
+    loadLogo();
+    std::cout << "Your playlist: " << std::endl;
+    loadPlaylist();
+    std::cout << "\t\t\t\tPick your color" <<std::endl;
+    std::cout << "\t\t\t\t0.Red 1.Yellow 2.Cyan 3.Blue 4.Black" <<std::endl;
+    int mood;
+    std::cin >> mood;
+    readEdgelist(g, mood);
+    makeAdj(g);
+        std::unordered_set<int> R, P, X;
+    for (unsigned i = 0; i < g.n(); i++) {
+        P.insert(i);
+    }
+
+    std::vector<std::unordered_set<int>> cliques;
+    bronKerboschPivot(g, R, P, X, cliques);
+    std::set<int> tracks;
+    for(auto &x: cliques)
+    {
+        for(auto &y: x)
+        {
+            tracks.insert(y);
+        }
+    }
+    std::vector<int> tracks_vec(tracks.begin(), tracks.end());
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::mt19937 gen(seed);
+    std::cout << "Songs we recommend...." << std::endl;
+    prev.clear();
+    for(unsigned i = 0; i < 5; i++)
+    {
+        std::uniform_int_distribution<int> dis(0, g.n()-1);
+        int index = dis(gen);
+        prev.push_back(index);
+        std::cout << i << " " << g.nodes[tracks_vec[index]].name << "-" << g.nodes[tracks_vec[index]].artist << std::endl;
+    }
+    std::cout << "5. Home" << std::endl;
+    std::cout << "6.exit" << std::endl;
+    return mood;
+
 }
 
 int main() {
@@ -245,33 +346,15 @@ int main() {
                 return 0;
             }
         }
-        if(!fileExists("fusedGraph.txt"))
-        {
-            std::cout << "File: fusedGraph.txt not found" << std::endl;
-            std::cout << "Fusing Graphs....." << std::endl;
-            int compile_status = system("g++ graph_fusion.cpp -o graph_fusion");
-            if(compile_status == 0)
-            {
-                int run_status = system("graph_fusion ");
-                if(run_status == 0)
-                {
-                    std::cout << "File: fusedGraph.txt created!" << std::endl;
-                }
-                else{
-                    std::cerr << "Error: Failed to execute graph_fusion program." << std::endl;
-                    return 0;
-                }
-            }
-            else 
-            {
-                std::cerr << "Error: Compilation of graph_fusion.cpp failed." << std::endl;
-                return 0;
-            }
-        }
+        
         
     }
     graph g;
     buildGraph(g);
+    std::vector<int>prev;
+    home(g, prev);
+
+    
 
     // Debug: Print nodes
     // std::cout << "Nodes:" << std::endl;
@@ -280,9 +363,9 @@ int main() {
     //     std::cout << n.name << ", " << n.danceability << ", " << n.energy << ", " << n.loudness << ", " << n.valence << std::endl;
     // }
 
-    int mood = 2;
-    readEdgelist(g, mood);
-    makeAdj(g);
+    // system("cls");
+    // loadLogo();
+    
 
     // Debug: Print edges
     // std::cout << "Edges:" << std::endl;
@@ -290,39 +373,22 @@ int main() {
     //     std::cout << g.nodes[e.source].name << " - " << g.nodes[e.target].name << std::endl;
     // }
 
-    std::unordered_set<int> R, P, X;
-    for (unsigned i = 0; i < g.n(); i++) {
-        P.insert(i);
-    }
 
-    std::vector<std::unordered_set<int>> cliques;
-    bronKerboschPivot(g, R, P, X, cliques);
-    std::set<int> tracks;
-    for(auto &x: cliques)
+    int trackid;
+    std::cout << "choose the songs you want to add to your playlist" << std::endl;
+    while(trackid != 6)
     {
-        for(auto &y: x)
+        std::cin >> trackid;
+        if(trackid == 5)
         {
-            tracks.insert(y);
+            //system("cls");
+            home(g, prev);
         }
-    }
-    std::vector<int> tracks_vec(tracks.begin(), tracks.end());
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::mt19937 gen(seed);
-    std::cout << "Here are your recommended songs..." << std::endl;
-    for(unsigned i = 0; i < 5; i++)
-    {
-        std::uniform_int_distribution<int> dis(0, g.n()-1);
-        int index = dis(gen);
-        std::vector<int> prev(5);
-        prev.push_back(index);
-        // for(auto &x: prev)
-        // {
-        //     if(index == x)
-        //     {
-        //         index = dis(gen);
-        //     }
-        // }
-        std::cout << g.nodes[tracks_vec[index]].name << "-" << g.nodes[tracks_vec[index]].artist << std::endl;
+        else
+        {
+            addToPlaylist(g, prev[trackid]);
+        }
+        
     }
 
     return 0;
