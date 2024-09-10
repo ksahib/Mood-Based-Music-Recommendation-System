552bb82f6f4eee229f708ad21419ac2a063b4088
Kazi Sahib <161735924+ksahib@users.noreply.github.com>
2024-09-10 13:07:05 +0600

    Update main.dart

--- UI/main.dart
+++ UI/main.dart
@@ -18,28 +18,205 @@ class Home extends StatelessWidget {
     body: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget> [
-          Center(
+           GestureDetector(
+            onTap: () {
+              // Navigate to suggestionWindowCyan
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => suggestionWindowRed()),
+              );
+            },
+            child: Center(
+              child: Container(
+                color: Colors.red,
+                margin: const EdgeInsets.all(10.0),
+                width: 150,
+                height: 150,
+              ),
+            ),
+          ),
+             GestureDetector(
+            onTap: () {
+              // Navigate to suggestionWindowCyan
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => suggestionWindowCyan()),
+              );
+            },
             child: Container(
+              color: Colors.cyan,
               margin: const EdgeInsets.all(10.0),
-              color: Colors.red,
               width: 150,
               height: 150,
             ),
           ),
-          Container(
-            margin: const EdgeInsets.all(10.0),
-            color: Colors.cyan,
-            width: 150,
-            height: 150,
+             
+              GestureDetector(
+            onTap: () {
+              // Navigate to suggestionWindowCyan
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => suggestionWindowBlue()),
+              );
+            },
+            child: Container(
+              color: Colors.blue[900],
+              margin: const EdgeInsets.all(10.0),
+              width: 150,
+              height: 150,
+            ),
           ),
-          Container(
-            margin: const EdgeInsets.all(10.0),
-            color: const Color.fromARGB(255, 96, 125, 139),
-            width: 150,
-            height: 150,
-          )
+          
         ],
       )
+    );
+  }
+}
+
+
+class suggestionWindowCyan extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Elevated Square'),
+        backgroundColor: Colors.black,
+      ),
+      body: Stack(
+        children: <Widget>[
+          // Full-screen background gradient
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment(1,-1.5),
+                end: Alignment(1,1),
+                colors: [
+                  Colors.cyan,
+                  Colors.black,
+                ],
+              ),
+            ),
+          ),
+          
+          // Elevated square centered on top of the background
+          
+            Card(
+              margin: EdgeInsets.all(25.0),
+              elevation: 10, // Adds elevation (shadow)
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0), // Rounded corners
+              ),
+              child: Container(
+                width: 150, // Width of the square
+                height: 150, // Height of the square
+                decoration: BoxDecoration(
+                  color: Colors.cyan, // Background color of the square
+                  borderRadius: BorderRadius.circular(8), // Same as card's border-radius
+                ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class suggestionWindowRed extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Elevated Square'),
+        backgroundColor: Colors.black,
+      ),
+      body: Stack(
+        children: <Widget>[
+          // Full-screen background gradient
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment(1,-1.5),
+                end: Alignment(1,1),
+                colors: [
+                  Colors.red,
+                  Colors.black,
+                ],
+              ),
+            ),
+          ),
+          
+          // Elevated square centered on top of the background
+          
+            Card(
+              margin: EdgeInsets.all(25.0),
+              elevation: 10, // Adds elevation (shadow)
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0), // Rounded corners
+              ),
+              child: Container(
+                width: 150, // Width of the square
+                height: 150, // Height of the square
+                decoration: BoxDecoration(
+                  color: Colors.red, // Background color of the square
+                  borderRadius: BorderRadius.circular(8), // Same as card's border-radius
+                ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class suggestionWindowBlue extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Elevated Square'),
+        backgroundColor: Colors.black,
+      ),
+      body: Stack(
+        children: <Widget>[
+          // Full-screen background gradient
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment(1,-1.5),
+                end: Alignment(1,1),
+                colors: [
+                  const Color.fromARGB(255, 13, 71, 161),
+                  Colors.black,
+                ],
+              ),
+            ),
+          ),
+          
+          // Elevated square centered on top of the background
+          
+            Card(
+              margin: EdgeInsets.all(25.0),
+              elevation: 10, // Adds elevation (shadow)
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0), // Rounded corners
+              ),
+              child: Container(
+                width: 150, // Width of the square
+                height: 150, // Height of the square
+                decoration: BoxDecoration(
+                  color: Colors.blue[900], // Background color of the square
+                  borderRadius: BorderRadius.circular(8), // Same as card's border-radius
+                ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
